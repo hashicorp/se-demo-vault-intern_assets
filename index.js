8d4d518a51e390618e0b046fc45df14bbe8775d1
hashanthony <anthony.benjamin@hashicorp.com>
2022-06-12 16:20:04 -0500

    added .env to .gitignore

--- index.js
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import Database from "./src/classes/Database.js";
-const database = new Database(process.env.MONGO_URL);
-
-database.createIntern(object, "Benjamin", "anthonyjbenjamin@gmail.com", "Pizza");
-
-// database.deleteIntern("anthonyjbenjamin@gmail.com");
