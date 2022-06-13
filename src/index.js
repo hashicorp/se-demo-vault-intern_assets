07175d50dd63a2a92680d42ed8169432e252283e
hashanthony <anthony.benjamin@hashicorp.com>
2022-06-12 19:35:57 -0500

    find intern modal done

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {fileURLToPath} from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/home.html"));
