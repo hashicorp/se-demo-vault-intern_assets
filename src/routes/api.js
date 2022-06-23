3071f1b485b42bce95c98fa99b0c555df9a58607
hashanthony <anthony.benjamin@hashicorp.com>
2022-06-23 13:59:15 -0500

    added npm start script

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const store = require("store2");
 const router = express.Router();
-const bodyParser = require("body-parser");
 
 const path = require("path");
 
