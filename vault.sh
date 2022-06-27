06b042fdacda32e00845b9c25a6a4739995c2718
hashanthony <anthony.benjamin@hashicorp.com>
2022-06-27 15:00:14 -0500

    bash script done

--- vault.sh
+++ vault.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-vault server -dev -dev-root-token-id=root
+# osascript -e "Terminal"
+#     activate
+#     do script "vault server -dev -dev-root-token-id=root"
+osascript -e "tell application \"Terminal\" to do script \"vault server -dev -dev-root-token-id=root\""
 export VAULT_ADDR=http://127.0.0.1:8200
 vault secrets enable transit
 vault write -f transit/keys/my-key
