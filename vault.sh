3b11b5e4080eb317676e087de9dab44103c49961
syedQuadri7 <syed.quadri@hashicorp.com>
2022-06-27 15:05:20 -0500

    added delay in bash script

--- vault.sh
+++ vault.sh
@@ -3,6 +3,7 @@
 #     activate
 #     do script "vault server -dev -dev-root-token-id=root"
 osascript -e "tell application \"Terminal\" to do script \"vault server -dev -dev-root-token-id=root\""
+sleep 3
 export VAULT_ADDR=http://127.0.0.1:8200
 vault secrets enable transit
 vault write -f transit/keys/my-key
